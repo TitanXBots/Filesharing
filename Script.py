c7afc6b5de8224ce8c07b3030eb70b45431bf3d6
Yash <titanmattersbot05@gmail.com>
2024-11-01 21:24:55 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,13 @@
+    LOG_TEXT = """<b>#NewUser
+    
+ID - <code>{}</code>
+
+Nᴀᴍᴇ - {}</b>
+"""
+    RESTART_TXT = """
+<b>Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ !
+
+📅 Dᴀᴛᴇ : <code>{}</code>
+⏰ Tɪᴍᴇ : <code>{}</code>
+🌐 Tɪᴍᴇᴢᴏɴᴇ : <code>Asia/Kolkata</code>
+🛠️ Bᴜɪʟᴅ Sᴛᴀᴛᴜs: <code>v2.7.1 [ Sᴛᴀʙʟᴇ ]</code></b>"""
